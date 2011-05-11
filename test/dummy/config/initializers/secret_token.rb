d94be8c859b8d33b7adbab0337e95f8558cea9f2
Spencer Steffen <spencer@citrusme.com>
2011-05-10 20:12:31 -0700

    remove test/dummy.. how ironic!

--- test/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '03d54866931c82850e3e723c3c43a30395d2190f9fbf2a0f69cf5a91a7871cdc888b75a39d9c2d6552bfef91f78fbec1a102959cc4b269bb058fea2548dcf686'
