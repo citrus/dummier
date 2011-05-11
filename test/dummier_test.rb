4c095dbcfc53d99e2117390340c97c8b6cc8b9be
Spencer Steffen <spencer@citrusme.com>
2011-05-10 18:03:56 -0700

    adding tests

---
+++ test/dummier_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class DummierTest < Test::Unit::TestCase
+
+  should "have dummier class" do
+    assert defined?(Dummier)
+  end
+
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,16 @@
+require 'bundler/setup'
+Bundler.require(:default, :test)
+require 'shoulda'
+
+
+class Test::Unit::TestCase
+
+  def setup
+    # nada 
+  end
+  
+  def teardown
+    # nada
+  end
+
+end
